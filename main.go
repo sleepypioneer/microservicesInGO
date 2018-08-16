3a020a2735d684c5973eac5a212ce4064a3485c2
Jessica <jessica0greene@gmail.com>
2018-08-16 13:15:10 +0200

    completion of part 2 & 3. Added seeding and Boltclient.

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 
+	"github.com/sleepypioneer/microservicesInGO/dbclient"
 	"github.com/sleepypioneer/microservicesInGO/services"
 )
 
-var appName = "accountservice"
+var appName = "MicroservicesInGO"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	initializeBoltClient()
 	services.StartWebServer("6767")
+}
+
+// Creates instance and calls the OpenBoltDb and Seed funcs
+func initializeBoltClient() {
+	services.DBClient = dbclient.BoltClient{}
+	services.DBClient.OpenBoltDb()
+	services.DBClient.Seed()
+	// Our microservice should now create a database on start.
 }
--- services/routes.go
+++ services/routes.go
@@ -21,9 +21,6 @@ var routes = Routes{
 		"GetAccount", // Name
 		"GET",        // HTTP method
 		"/accounts/{accountId}", // Route pattern
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-			w.Write([]byte("{\"result\":\"OK\"}"))
-		},
+		GetAccount,
 	},
 }
